17e44826ceb460216c11f14a6fe7969612018974
mehmet colak <mc270420@gmail.com>
2023-04-20 12:44:18 +0300

    change the security and using md5

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 const app = express();
 
 
@@ -21,7 +20,6 @@ const userSchema = new mongoose.Schema({
 }); 
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET_KEY ,encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema); 
 
@@ -40,7 +38,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",async (req,res)=>{
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     await newUser.save()
@@ -59,7 +57,7 @@ app.post("/register",async (req,res)=>{
 
 app.post("/login",async (req,res)=>{
 const username = req.body.username;
-const password = req.body.password;
+const password = md5(req.body.password);
   
 
     await User.findOne({email:username})
