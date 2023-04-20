bc9dcbdfc143437e8f9e8c6387235703acf4d927
mehmet colak <mc270420@gmail.com>
2023-04-20 13:50:43 +0300

    using better security way bcrypt and salt method

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 
@@ -36,38 +38,45 @@ app.get("/register",(req,res)=>{
 
 
 app.post("/register",async (req,res)=>{
-    const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
+     
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(req.body.password, salt,async function(err, hash) {
+            const newUser = new User({
+                email:req.body.username,
+                password:hash
+            });
+        
+            await newUser.save()
+            .then(user => {
+                if(user){
+                    res.render("secrets");
+                }else{
+                    console.log(user);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        });
     });
-
-    await newUser.save()
-    .then(user => {
-        if(user){
-            res.render("secrets");
-        }else{
-            console.log(user);
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    })
+    
 })
 
 
 app.post("/login",async (req,res)=>{
 const username = req.body.username;
-const password = md5(req.body.password);
-  
+const password = req.body.password
 
     await User.findOne({email:username})
     .then(foundUser => {
         if(foundUser){
-            if(foundUser.password === password){
-                res.render("secrets");
-            }else{
-                res.send("not found or wrong password");
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true){
+                    res.render("secrets");
+                }else{
+                    res.send("wrong email or password")
+                }
+            });
         }else{
             console.log(user);
         }
